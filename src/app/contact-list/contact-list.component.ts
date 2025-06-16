45b68dc8651d04b271a76b2d01d9e8b5198650b2
triceratops777 <jgood.ulysses@gmail.com>
2025-06-16 12:25:01 +0200

    upd

--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -1,13 +1,15 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { RouterModule } from '@angular/router';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
-import {MatMenuModule} from '@angular/material/menu';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-contact-list',
@@ -19,28 +21,31 @@ import {MatMenuModule} from '@angular/material/menu';
     FormsModule, //Necessaire pour la directive ngModel (template driven form)
     MatIconModule,
     MatButtonModule,
-    CommonModule, MatMenuModule, 
+    CommonModule, MatMenuModule,
+    MatDividerModule,
+    MatSnackBarModule,
   ],
   templateUrl: './contact-list.component.html',
   styleUrl: './contact-list.component.css'
 })
-export class ContactListComponent implements OnInit  {
+export class ContactListComponent implements OnInit {
+  constructor(private snackBar: MatSnackBar) { };
   //A:
   //Creer variable tableau "listeContact"
   //Qui contient deux contactes moi et ma camarade
   //avec "nom" et "prenom"
   listeContact = [
     {
-      id:7, prenom: 'Tarek', nom: 'Najem'
+      id: 7, prenom: 'Bob', nom: 'Bobertus', email: 'bobertus@gmail.com'
     },
     {
-      id:88, prenom: 'Tarek 2', nom: 'Najem 2'
+      id: 88, prenom: 'Anna', nom: 'Malina', email: 'anmalina@gmail.com'
     },
     {
-      id:9, prenom: 'A....', nom: 'Najem 2'
+      id: 5, prenom: 'Simo', nom: 'Bernardo', email: 'sbernardo@gmail'
     },
     {
-      id:9, prenom: 'B.....', nom: 'Najem 2'
+      id: 9, prenom: 'Alex', nom: 'Chat', email: 'alexchat@gmail.com'
     }
   ];
 
@@ -50,31 +55,41 @@ export class ContactListComponent implements OnInit  {
 
   //intiliasé tableau vide pour eviter les exceptions de Null
   //Quand j'essaie d'acceder
-  listeContactFiltre : any[] = [];
+  listeContactFiltre: any[] = [];
 
   countContacts = 0;
-  textRecherche : string = '';
+  textRecherche: string = '';
   selectedId: number | null = null;
   menuOpenedId: number | null = null;
-menu: any;
+  menu: any;
 
   //C:
   //Afficher le nombre de contactes au bas de la page
-  
+
   ngOnInit() {
     this.onSearch();
 
     //Compter le nombre des elements
     //methode 1: .length;
     //this.countContacts = this.listeContact.length;
- 
+
     //methode 2: boucle for
     for (let index = 0; index < this.listeContact.length; index++) {
-      this.countContacts = this.countContacts + 1;     
+      this.countContacts = this.countContacts + 1;
     }
   }
 
+
+  sendEmail(email: string): void {
+    window.location.href = 'mailto:' + email;
+
+  }
+
+
+
   onSearch() {
+
+    this.listeContact = this.listeContact.sort((a, b) => a.prenom.localeCompare(b.prenom));
     //console.log(this.textRecherche);
 
     //Methode 1: par filter
@@ -102,21 +117,24 @@ menu: any;
     //Methode 3: avec la boucle for of
     //pas acces à l'indexe
     this.listeContactFiltre = [];
-    for(let element of this.listeContact) {
-      const recherche= this.textRecherche.toLowerCase().trim();
-      if(
-        element.nom.toLowerCase().startsWith(recherche)|| 
-        element.prenom.toLowerCase().startsWith(recherche) ) {
+    for (let element of this.listeContact) {
+      const recherche = this.textRecherche.toLowerCase().trim();
+
+
+      if (
+        element.nom.toLowerCase().startsWith(recherche) ||
+        element.prenom.toLowerCase().startsWith(recherche)) {
         this.listeContactFiltre.push(element);
       }
     }
-
   }
 
-  onDelete(id:number) {
-    const index = this.listeContact.findIndex(item=>item.id==id)
-    this.listeContact.splice(index,1);
-    this.onSearch()
-
+  onDelete(id: number) {
+    const index = this.listeContact.findIndex(item => item.id == id)
+    this.listeContact.splice(index, 1);
+    this.onSearch();
+    this.snackBar.open('Deleted');
   }
+
 }
+
